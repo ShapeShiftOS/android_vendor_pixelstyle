954302b9232a05a245bb7a6beb79a595f13bc4e1
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-02 21:56:44 -0300

    Drop GooglePermissionControllerOverlay
    Not used anymore
    
    Change-Id: I4909b5b38085c66e663577da14e36d5c52c1d954

--- config.mk
+++ config.mk
@@ -21,8 +21,7 @@ PRODUCT_PACKAGES += \
     PixelThemesStub2019 \
     OPScreenRecorder \
     SafetyHubPrebuilt \
-    SettingsIntelligenceGooglePrebuilt \
-    GooglePermissionControllerOverlay
+    SettingsIntelligenceGooglePrebuilt
 
 ifeq ($(TARGET_GAPPS_ARCH),arm64)
 PRODUCT_PACKAGES += \
