DerpLab/platform_vendor_pixelstyle

pixelstyle: fonts: Add Samsung One font [2/2]
Change-Id: Iad1ec93aaa83d4b1e8ed792502b08e0c4853fe70
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

config.mk
@@ -91,6 +91,7 @@ PRODUCT_COPY_FILES += \
     vendor/pixelstyle/fonts/PhantomBold/PhantomBold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/PhantomBold-Regular.ttf \
     vendor/pixelstyle/fonts/snowstorm/snowstorm.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Snowstorm-Regular.ttf \
     vendor/pixelstyle/fonts/vcrosd/vcr_osd_mono.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/ThemeableFont-Regular.ttf \
+    vendor/pixelstyle/fonts/SamsungOne.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SamsungOne.ttf \
     vendor/pixelstyle/fonts/Shamshung/Shamshung.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Shamshung.ttf \
     vendor/pixelstyle/fonts/Aclonica.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Aclonica.ttf \
     vendor/pixelstyle/fonts/Amarante.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Amarante.ttf \

ShapeShiftOS/android_vendor_pixelstyle

config.mk
@@ -224,3 +224,4 @@ PRODUCT_COPY_FILES += \
     vendor/pixelstyle/fonts/fontage/SofiaSansSemiCond-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/SofiaSansSemiCond-Medium.ttf
     vendor/pixelstyle/fonts/EvolveSans.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/EvolveSans.ttf \
     vendor/pixelstyle/fonts/Linotte.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Linotte.ttf \
+    vendor/pixelstyle/fonts/SamsungOne.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/SamsungOne.ttf \